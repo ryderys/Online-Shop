57f397da43084a3f55df5eb1a108db9881b2bb10
ryderys <youssefi.ashkan.ys@gmail.com>
2024-07-14 14:51:28 +0330

    addedd database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv")
 const app = express();
 dotenv.config()
+require("./config/database.config")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
