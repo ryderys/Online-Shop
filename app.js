2bfff959f621a20c879b2fa434bf86f7f40d8ac6
ryderys <youssefi.ashkan.ys@gmail.com>
2024-07-14 19:57:40 +0330

    updated the structure of the project and added swagger

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv")
 const app = express();
+const swaggerSetup = require("./src/config/swagger.config")
 dotenv.config()
-require("./config/database.config")
+require("./src/config/database.config")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+swaggerSetup(app)
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port 3000");
 })
