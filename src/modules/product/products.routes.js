6c56d35c7a06d449f1b958c9bed4c61584b0e2f7
ryderys <youssefi.ashkan.ys@gmail.com>
2024-07-24 22:23:37 +0330

    added product router permissions

--- src/modules/product/products.routes.js
+++ src/modules/product/products.routes.js
@@ -6,11 +6,11 @@ const { ProductController } = require("./product.controller")
 
 const router = require("express").Router()
 
-router.post("/add", Authorization,  uploadFile.array("images", 10) ,stringToArray("tags"), ProductController.addProduct)
-router.get("/all", Authorization,  ProductController.getAllProducts)
-router.get("/:id",Authorization,  ProductController.getOneProductById)
-router.delete("/remove/:id",Authorization,  ProductController.removeProductById)
-router.patch("/edit/:id",Authorization,  uploadFile.array("images", 10), stringToArray("tags"), ProductController.editProductById)
+router.post("/add", Authorization, checkPermission('product', 'create'), uploadFile.array("images", 10) ,stringToArray("tags"), ProductController.addProduct)
+router.get("/all", Authorization, checkPermission('product', 'read'), ProductController.getAllProducts)
+router.get("/:id",Authorization, checkPermission('product', 'read'), ProductController.getOneProductById)
+router.delete("/remove/:id",Authorization, checkPermission('product', 'delete'), ProductController.removeProductById)
+router.patch("/edit/:id",Authorization, checkPermission('product', 'update'), uploadFile.array("images", 10), stringToArray("tags"), ProductController.editProductById)
 
 module.exports = {
     AdminApiProductRouter : router
