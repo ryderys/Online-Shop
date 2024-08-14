d8e83fb2444e06094d99bf054d3eb6c58a6b81ca
ryderys <youssefi.ashkan.ys@gmail.com>
2024-08-14 11:59:48 +0330

    added database connection

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,9 +1,17 @@
 const { default: mongoose } = require("mongoose")
+const {MongoClient} = require("mongodb")
 const dotenv = require("dotenv")
 dotenv.config()
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("connected to DB");
-}).catch(err => {
-    console.log(err?.message ?? "failed to connect to DB");
-})
+// mongoose.connect(process.env.MONGODB_URL).then(() => {
+//     console.log("connected to DB");
+// }).catch(err => {
+//     console.log(err?.message ?? "failed to connect to DB");
+// })
+
+mongoose.connect('mongodb+srv://youssefiashkanys:Ashkanys79@first-project.co830.mongodb.net/?retryWrites=true&w=majority&appName=first-project').then(() => {
+        console.log("connected DB");
+    }).catch(err => {
+        console.log(err);
+    })
+
