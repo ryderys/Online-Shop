3737152e9772405d6854f0ca7bf5c55aa35be851
Ali Khalaj <alikhalaj779@gmail.com>
2025-04-21 18:25:02 +0330

    mui setting for project done

--- frontend/src/mui/theme.js
+++ frontend/src/mui/theme.js
@@ -1,15 +1,44 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-    typography:{
-        fontFamily: `"Vazir" , "YekanBakh" , "Roboto" , "Arial"`,
-        fontWeightLight: 100 , 
-        fontWeightRegular: 200 , 
-        fontWeightMedium: 300 , 
-        fontWeightBold: 400 , 
-        fontWeightHeavy: 600,
+  typography: {
+    fontFamily: `"Vazir" , "YekanBakh" , "Roboto" , "Arial"`,
+    fontWeightLight: 100,
+    fontWeightRegular: 200,
+    fontWeightMedium: 300,
+    fontWeightBold: 400,
+    fontWeightHeavy: 600,
+  },
+  direction: "rtl",
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          margin: 0,
+          padding: 0,
+          boxSizing: "border-box",
+        },
+        html: {
+          margin: 0,
+          padding: 0,
+        },
+      },
     },
-    direction: "rtl"
-})
+
+    MuiContainer: {
+      defaultProps: {
+        disableGutters: true, // حذف پدینگ داخلی پیش‌فرض کانتینر
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          margin: 0,
+          padding: 0,
+        },
+      },
+    },
+  },
+});
 
 export default theme;
