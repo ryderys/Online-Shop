c02b904f5251807a8721ec9add9b8dca95d7890f
Ali Khalaj <alikhalaj779@gmail.com>
2025-04-29 18:10:06 +0330

    react router dom added to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 //imports for reactQuery
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import Router from "./router/Router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Router from "./router/Router";
 import defaultOptions from "./configs/reactQuery";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Layout from "./pages/Layout";
 
 function App() {
   //create client to use reactQuery
-  const queryClient = new QueryClient({defaultOptions});
+  const queryClient = new QueryClient({ defaultOptions });
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
-      <Router />
+        <Router />
       </Layout>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
 
-export default App
+export default App;
