4d523cd712a67647dad9294412c36bab472dac58
Ali Khalaj <alikhalaj779@gmail.com>
2025-05-12 19:02:58 +0330

    HomePage done

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,20 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-// import AuthPage from './AuthPage';
-import { Link } from 'react-router-dom';
-import { getUserProfile } from '../services/users';
+import Banner from '../components/templates/homePage/Banner';
+import CategorySlider from '../components/templates/homePage/CategorySlider';
+import ProductsSection from '../components/templates/homePage/ProductsSection';
+import Video from '../components/templates/homePage/Video';
+import SecondProductSection from '../components/templates/homePage/SecondProductSection';
 
 const Homepage = () => {
-     const { data } = useQuery({queryKey: ["profile"],queryFn: getUserProfile,});
 
     return (
       <div>
-        {data ? <Link to="/user">userPage</Link> : null}
-        <br />
-        {data && data.data.data.user.role=== "admin"?<Link to="/admin">adminPage</Link>:null}
-        <h3>HomePage</h3>
-        <Link to="/login">login</Link>
-        {/* <AuthPage /> */}
+        <Banner />
+        <CategorySlider />
+        <ProductsSection />
+        <Video />
+        <SecondProductSection />
       </div>
     );
 };
